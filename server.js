a9d8121a6afd72d144f1736d81cf63fd030d1985
Andrew Kolander <akolander22@gmail.com>
2016-10-08 21:50:32 -0500

    basic server set up

---
+++ server.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var passport = require('passport');
+var path = require('path');
+
+
+
+var app = express();
+
+
+app.get('/', function(request, response){
+  response.sendFile(path.join(__dirname, 'public/views/index.html'));
+})
+
+
+
+
+
+
+
+var server = app.listen(process.env.PORT || 3000, function(){
+  var port = server.address().port;
+  console.log('Listening on port', port);
+})
