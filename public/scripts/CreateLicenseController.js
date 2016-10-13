7112e9a7711d1a54a84df5f77dcc9f346ba17caa
Andrew Kolander <akolander22@gmail.com>
2016-10-13 12:32:36 -0500

    added form to make new license

--- public/scripts/CreateLicenseController.js
+++ public/scripts/CreateLicenseController.js
@@ -1,16 +1,20 @@
 angular.module('educationApp').controller('CreateLicenseController',['$http','$mdDialog', '$mdMedia', function($http,$mdDialog,$mdMedia){
   var vm = this;
 
+  vm.submit = function(){
+    console.log('hit submit', vm.duedate);
+
+    var sendData = {};
+    sendData.name = vm.name;
+    sendData.number = vm.number;
+    sendData.duedate = vm.duedate;
+    sendData.hoursReq = vm.hoursReq;
+    sendData.hours = vm.hours;
+
+    console.log(sendData);
+    $http.post()
+  }
 
 
-  // var useFullScreen = ($mdMedia('sm') || $mdMedia('xs'));
-  // $mdDialog.show({
-  //   controller: 'CreateLicenseController',
-  //   controllerAs: 'create',
-  //   templateUrl: 'views/license.html',
-  //   fullscreen: useFullScreen,
-  //   clickOutsideToClose: true,
-  //   ariaLabel: 'Good',
-  // })
 
 }]);
--- public/scripts/MyLicensesController.js
+++ public/scripts/MyLicensesController.js
@@ -1,18 +1,18 @@
 angular.module('educationApp').controller('MyLicensesContoller', function($http,$mdMedia,$mdDialog){
   var vm = this;
 
-  vm.addLicense = function() {
-    console.log('button pressed');
+  vm.newLicense = function(){
+    console.log('hello');
     var useFullScreen = ($mdMedia('sm') || $mdMedia('xs'));
     $mdDialog.show({
       controller: 'CreateLicenseController',
       controllerAs: 'create',
-      templateUrl: 'views/license.html',
+      templateUrl: 'views/createlicense.html',
       fullscreen: useFullScreen,
       clickOutsideToClose: true,
-      ariaLabel: 'Form',
+      ariaLabel: 'Good',
     })
-}
+  }
 
 
 })
